f6179cdd62089287699abaffc365ab8ed7ea02e3
Nelson Gallardo <gnelson85@gmail.com>
2015-01-24 12:37:50 +0000

    websockets stuff

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
-var app = require('express')();
+var express = require('express')
+var app = express()
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
     res.sendfile('index.html');
 });
+
+app.get('/test', function(req, res){
+    res.sendFile(__dirname + '/ui/application.html')
+});
+
+app.use(express.static((__dirname + '/ui')));
+
 
 var ws = require('./ws')(io)
 
