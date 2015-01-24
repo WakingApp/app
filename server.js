54dd64282bef8e4f30b415fc81e855a0ca0d9ae5
Nelson Gallardo <gnelson85@gmail.com>
2015-01-24 13:42:24 +0000

    webscockets

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ var app = express()
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+
 app.get('/', function(req, res){
     res.sendfile('index.html');
 });
+
+io.emit('settings', {alarmTime: "", timeRange:"0" });
 
 app.get('/test', function(req, res){
     res.sendFile(__dirname + '/ui/application.html')
@@ -14,7 +17,12 @@ app.get('/test', function(req, res){
 app.use(express.static((__dirname + '/ui')));
 
 
-var ws = require('./ws')(io)
+var ws = require('./ws')
+
+ws.ws(io)
+
+console.log("Alarm Time: " + ws.alarmTime)
+console.log("Time Range: " + ws.timeRange)
 
 http.listen(3000, function(){
     console.log('listening on *:3000');
--- ws.js
+++ ws.js
@@ -1,10 +1,20 @@
+var alarmTime = '0'
+var timeRange = 0
 
-
-module.exports = function(io){
+module.exports.ws = function(io){
     io.on('connection', function(socket){
+        io.emit('settings', {alarmTime: alarmTime, timeRange:timeRange });
+        socket.on('settings', function(msg){
+            alarmTime = msg.alarmTime
+            timeRange = msg.timeRange
+            //console.log('Alarm time: ' +  alarmTime)
+            //console.log('Time range: ' +  timeRange)
+        });
         socket.on('alarmInfo', function(msg){
-            //io.emit('chat message', msg);
             console.log('The message: ' +  msg)
         });
     });
 }
+
+module.exports.alarmTime = alarmTime
+module.exports.timeRange = timeRange
