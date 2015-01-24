1bcc4960be961584f5b84a2ab4bb15811e0e3f1b
Nelson Gallardo <gnelson85@gmail.com>
2015-01-24 23:03:19 +0000

    crazy stuff

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var express = require('express')
 var app = express()
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
+var showerOccupiedAlertSent = false;
+var showerFreeAlertSent = false;
 
 app.get('/', function(req, res){
     res.sendfile('index.html');
@@ -16,8 +17,33 @@ app.get('/test', function(req, res){
 
 app.use(express.static((__dirname + '/ui')));
 
-var ws = require('./ws')(io)
+var iotEventDispatcher = require('./iotEventDispatcher')(io)
+
+var ws = require('./ws')(io, iotEventDispatcher)
+
+setInterval(function(){
+    var currentTime = new Date().getTime();
+    if(Math.abs(currentTime - ws.alarmTimeFormatted) < 30 * 60 * 1000){
+        var nearEvent = ws.getNearEvents(ws.alarmTimeFormatted)
+        if(iotEventDispatcher.isShowerOccupied && !showerOccupiedAlertSent){
+            io.emit('showerOccupied', "");
+            showerOccupiedAlertSent = true
+        }else if(!iotEventDispatcher.isShowerOccupied && !showerFreeAlertSent){
+            var timeLapsed = currentTime - ws.alarmTimeFormatted
+            console.log("timeLapsed " + timeLapsed)
+            if(timeLapsed > 0) {
+                io.emit('showerFree', "");
+                io.emit('alarm', true);
+                showerFreeAlertSent = true
+                //ws.alarmFired = true
+            }
+
+        }
+    }
+}, 1000)
 
 http.listen(3000, function(){
+    var shower = require('./lib/shower')(iotEventDispatcher.showerOn.bind(this), iotEventDispatcher.showerOff.bind(this));
+    shower();
     console.log('listening on *:3000');
 });
